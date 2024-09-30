e3d521aef04ea32c9b133af977878491d9bec8f2
“Latania <latania.reece@outlook.com>
2024-09-30 13:23:58 -0400

    fix hero shape gradient

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,13 +47,13 @@ const HeroSection = () => {
 
       {/* Mobile bottom shapes */}
       <div className="absolute inset-0 justify-center items-center lg:hidden z-0">
-        <div className="w-4/5 h-2/5 transform rotate-6 bg-gradient-to-r from-primaryColor via-primaryColorLight to-defaultBg opacity-5 border-2 border-slate-50 absolute -left-15 bottom-10" />
+        <div className="w-4/5 h-2/5 transform rotate-6 bg-gradient-to-r from-primaryColor to-defaultBg opacity-5 border-2 border-slate-50 absolute -left-15 bottom-10" />
         <div className="w-3/5 h-1/3 transform -rotate-6 bg-gradient-to-r from-secondaryColorLight to-secondaryColorDark opacity-5 border-2 border-slate-100 absolute -left-5 bottom-5" />
       </div>
 
       {/* Desktop right shapes */}
       <div className="absolute top-0 right-0 h-full w-1/2 justify-end items-center hidden lg:flex">
-        <div className="w-2/3 h-2/3 transform rotate-12 bg-gradient-to-r from-primaryColor via-primaryColorLight to-defaultBg opacity-50 border-2 border-slate-50" />
+        <div className="w-2/3 h-2/3 transform rotate-12 bg-gradient-to-r from-primaryColor to-defaultBg opacity-50 border-2 border-slate-50" />
         <div className="w-1/3 h-1/3 transform -rotate-12 absolute bottom-16 right-0 bg-gradient-to-r from-secondaryColorLight to-secondaryColorDark opacity-60 border-2 border-slate-100" />
       </div>
     </section>
